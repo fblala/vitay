5c4696bae5f76103768dc33acde36d82826f2844
haileynegi <haileynegi@gmail.com>
2018-04-27 12:05:29 -0400

    button learn more

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,12 @@
-$("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".second").offset().top},
-        'slow');
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
 });
