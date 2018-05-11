2d5c115862e643730f4c417ded5a91aaf5a62b4c
haileynegi <haileynegi@gmail.com>
2018-05-10 22:24:05 -0400

    the entire fucking website

--- scroll.js
+++ scroll.js
@@ -10,3 +10,8 @@ $('a[href^="#"]').on('click', function(event) {
     }
 
 });
+<img src="images/resume1.png" class="resumetemp"alt="pic">
+  <a href="https://www.behance.net/gallery/16700687/Free-resume-template-CV" class="btn-5">Download</a>
+
+  If you are seeking full-time or freelance work then you will know the power of having a good professional looking resume or CV: It helps you stand out from the crowd of boring resumes created in Microsoft Word using standard fonts. A well-designed resume with good content adds that final bit of polish to your job application.
+  However, not everyone is a designer so if you try creating something more unique yourself you run the risk of creating a poor layout and weak font choices. That is where this post is here to help you – professional designers have created thousands of free resume templates which you can download and customize in your program of choice</p>
